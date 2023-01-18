16a899c0c9f92942c30e8129ad33aaf6b3f20a9f
Natael S <96678889+natael221@users.noreply.github.com>
2023-01-18 08:18:35 +0700

    Add files via upload
    UAS

---
+++ pa_web (1).sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 18, 2023 at 02:17 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pa_web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_menu`
+--
+
+CREATE TABLE `daftar_menu` (
+  `id_menu` int(6) NOT NULL,
+  `menu` varchar(255) NOT NULL,
+  `harga` double(20,2) NOT NULL,
+  `path` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `daftar_menu`
+--
+
+INSERT INTO `daftar_menu` (`id_menu`, `menu`, `harga`, `path`) VALUES
+(1, 'Bakso', 13000.00, '../static/img/bakso.jpeg'),
+(2, 'Mie ayam', 15000.00, '../static/img/mieayam.jpg'),
+(4, 'Sushi', 150000.00, '../static/img/a0000370_main.png'),
+(5, 'Nasi Goreng', 20000.00, '../static/img/nasgor.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meja`
+--
+
+CREATE TABLE `meja` (
+  `no_meja` int(2) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `status_meja` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `meja`
+--
+
+INSERT INTO `meja` (`no_meja`, `keterangan`, `status_meja`) VALUES
+(1, 'Indoor', 'Terisi'),
+(2, 'indoor', 'Kosong'),
+(3, 'Indoor', 'Kosong'),
+(4, 'Indoor', 'Kosong');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promo`
+--
+
+CREATE TABLE `promo` (
+  `id_promo` int(255) NOT NULL,
+  `menu` varchar(255) NOT NULL,
+  `harga_awal` double(10,2) NOT NULL,
+  `harga_promo` double(10,2) NOT NULL,
+  `tanggal` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `promo`
+--
+
+INSERT INTO `promo` (`id_promo`, `menu`, `harga_awal`, `harga_promo`, `tanggal`) VALUES
+(1, 'Bakso', 13000.00, 10000.00, '2023-01-12'),
+(7, 'Sushi', 150000.00, 120000.00, '2023-01-11'),
+(8, 'Mie ayam', 15000.00, 12000.00, '2023-01-12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservasi`
+--
+
+CREATE TABLE `reservasi` (
+  `id_pemesanan` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `telp` varchar(15) DEFAULT NULL,
+  `jum_tamu` int(3) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `jam` time NOT NULL,
+  `tambahan` varchar(255) DEFAULT NULL,
+  `id_user` int(11) NOT NULL,
+  `meja_no` int(11) NOT NULL,
+  `ket_meja` varchar(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL,
+  `pesan_menu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservasi`
+--
+
+INSERT INTO `reservasi` (`id_pemesanan`, `nama`, `email`, `telp`, `jum_tamu`, `tanggal`, `jam`, `tambahan`, `id_user`, `meja_no`, `ket_meja`, `status`, `pesan_menu`) VALUES
+(1, 'aryaputra', 'kinsglaive34@gmail.com', '0127777', 3, '2023-01-05', '07:41:48', 'Bangku Bayi', 2, 2, 'indoor', 'Selesai', ''),
+(2, 'Deni', 'email@email.com', '321321', 2, '2023-01-04', '22:03:10', 'tidak ada', 2, 3, 'indoor', 'Selesai', ''),
+(7, 'Sagiri', 'Sagiri@gmail.com', '08154845898', 2, '2023-01-11', '21:33:00', 'Tidak ada', 2, 1, 'Indoor', 'Pending', ''),
+(8, 'Angelina Jolie', 'dwasdw@gmail.com', '081546578456', 2, '2023-01-11', '21:34:00', 'Tidak ada', 2, 4, 'Indoor', 'Selesai', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
+(1, 'admin', '321'),
+(2, 'Sagiri', '1234'),
+(4, 'Welt', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `daftar_menu`
+--
+ALTER TABLE `daftar_menu`
+  ADD PRIMARY KEY (`id_menu`),
+  ADD KEY `menu` (`menu`),
+  ADD KEY `harga` (`harga`);
+
+--
+-- Indexes for table `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`no_meja`),
+  ADD KEY `keterangan` (`keterangan`);
+
+--
+-- Indexes for table `promo`
+--
+ALTER TABLE `promo`
+  ADD PRIMARY KEY (`id_promo`),
+  ADD KEY `harga_awal` (`harga_awal`),
+  ADD KEY `menu` (`menu`);
+
+--
+-- Indexes for table `reservasi`
+--
+ALTER TABLE `reservasi`
+  ADD PRIMARY KEY (`id_pemesanan`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `meja_no` (`meja_no`),
+  ADD KEY `ket_meja` (`ket_meja`),
+  ADD KEY `pesan_menu` (`pesan_menu`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `daftar_menu`
+--
+ALTER TABLE `daftar_menu`
+  MODIFY `id_menu` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `no_meja` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `promo`
+--
+ALTER TABLE `promo`
+  MODIFY `id_promo` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `reservasi`
+--
+ALTER TABLE `reservasi`
+  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `promo`
+--
+ALTER TABLE `promo`
+  ADD CONSTRAINT `promo_ibfk_1` FOREIGN KEY (`harga_awal`) REFERENCES `daftar_menu` (`harga`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `promo_ibfk_2` FOREIGN KEY (`menu`) REFERENCES `daftar_menu` (`menu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reservasi`
+--
+ALTER TABLE `reservasi`
+  ADD CONSTRAINT `reservasi_ibfk_1` FOREIGN KEY (`meja_no`) REFERENCES `meja` (`no_meja`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservasi_ibfk_2` FOREIGN KEY (`ket_meja`) REFERENCES `meja` (`keterangan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservasi_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
